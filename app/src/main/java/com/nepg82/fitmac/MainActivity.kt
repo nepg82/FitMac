a092d25fb629a74d6650c1be4fb2284470509c46
nepg82 <nepg82@gmail.com>
2026-07-20 19:33:16 -0400

    Test Room database persistence

--- app/src/main/java/com/nepg82/fitmac/MainActivity.kt
+++ app/src/main/java/com/nepg82/fitmac/MainActivity.kt
@@ -4,44 +4,77 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.lifecycleScope
+import com.nepg82.fitmac.data.database.entities.Meal
 import com.nepg82.fitmac.ui.theme.FitMacTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+
+    private val database by lazy {
+        (application as FitMacApplication).database
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             FitMacTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+
+                    MealTestScreen(
                         modifier = Modifier.padding(innerPadding)
                     )
+
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    @Composable
+    private fun MealTestScreen(
+        modifier: Modifier = Modifier
+    ) {
+        val meals by database.mealDao()
+            .getAllMeals()
+            .collectAsState(initial = emptyList())
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FitMacTheme {
-        Greeting("Android")
+        Column(
+            modifier = modifier.padding(16.dp)
+        ) {
+
+            Button(
+                onClick = {
+                    lifecycleScope.launch {
+                        database.mealDao().insertMeal(
+                            Meal(
+                                name = "Test Breakfast"
+                            )
+                        )
+                    }
+                }
+            ) {
+                Text("Add Test Meal")
+            }
+
+            meals.forEach { meal ->
+                Text(
+                    text = meal.name,
+                    modifier = Modifier.padding(top = 8.dp)
+                )
+            }
+        }
     }
 }
