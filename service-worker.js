d3b5f4bedf0cc8cc54b6c25d92b4599e11b30b22
nepg82 <nepg82@gmail.com>
2026-08-28 21:52:30 -0400

    Update icon paths in service worker

--- service-worker.js
+++ service-worker.js
@@ -13,8 +13,8 @@ const ASSETS = [
   './js/workout.js',
   './js/sync.js',
   './js/app.js',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
+  './app-icons/app-icon-192.png',
+  './app-icons/app-icon-512.png'
 ];
 
 self.addEventListener('install', (event) => {
